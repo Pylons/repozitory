bfcfb3cbe28594f712c21dc7b63ee64a5100a720
Shane Hathaway <shane@hathawaymix.org>
2012-01-15 02:54:18 -0700

    Never mind, no release for now. Karl bug did not get fixed.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.1'
+version = '1.1dev'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
